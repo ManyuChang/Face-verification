0ad95d231593bf4247faa92fa9288329de4d5be4
ManyuChang <806706917@qq.com>
2017-01-06 20:55:40 +0800

    Update compare_test.sh

--- src/compare_test.sh
+++ src/compare_test.sh
@@ -1,9 +1,9 @@
-cd ../data/image
+cd ../data/images
 files=$(ls -v)
 
 cd ../../src
 for file in $files; do	
 
-	python compare.py ./models/20161116-234200 ../data/image/$file/2.jpg ../data/image/$file/1.jpg
+	python compare.py ./models/20161116-234200 ../data/images/$file/2.jpg ../data/images/$file/1.jpg
 
 done
